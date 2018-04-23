41f50270f8d6cf86982d95f3f75b16c8e11976b8
piotrfatner <fatner.piotr@gmail.com>
2018-04-23 20:17:27 +0200

    Scheduler. Personalizacja.

--- src/main/resources/static/js/visitAppointment.js
+++ src/main/resources/static/js/visitAppointment.js
@@ -1,4 +1,4 @@
-app.controller('visitAppointmentController', function($scope,$http,$window,$location) {
+app.controller('visitAppointmentController', function($scope,$http,$window,$location,uiCalendarConfig) {
     $scope.today = new Date();
     var date = new Date();
     var d = date.getDate();
@@ -13,6 +13,7 @@ app.controller('visitAppointmentController', function($scope,$http,$window,$loca
             { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2) },
             { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false },
             { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false },
+            { title: 'test123', start: new Date(2018, 3, 24, 10, 30, 0, 0), end: new Date(2018, 3, 24, 11, 30, 0, 0)},
             { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false },
             { title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/' }
         ]
@@ -21,13 +22,19 @@ app.controller('visitAppointmentController', function($scope,$http,$window,$loca
 
     $scope.uiConfig = {
         calendar: {
+            defaultView:'agendaWeek',
+
+            minTime: "08:00:00",
+            maxTime: "17:00:00",
+            weekends: false,
             height: 400,
             firstDay:1,
             locale: 'pl',
             lang: 'pl',
+            timezone:false,
             editable: true,
             header: {
-                left: 'month,basicWeek,basicDay',
+                left: 'agendaWeek,agendaDay',
                 center: 'title',
                 right: 'prev,next today'
             },
@@ -38,18 +45,29 @@ app.controller('visitAppointmentController', function($scope,$http,$window,$loca
             selectable: true,
             selectHelper: true,
             unselectAuto: true,
-            select: function (start, end) {
+            select: function (start, end, moment) {
                 var title = prompt('Event Title:');
                 var eventData;
+                console.log("start:");
+                console.log(start._d.getUTCDate());
+                console.log("end:");
+                console.log(end);
+                console.log("another:");
+                console.log(moment);
                 if (title) {
                     eventData = {
                         title: title,
-                        start: start.format(),
-                        end: end.format()
+                        start: new Date(2018, 4, 23, 10, 33, 0, 0),
+                        end: new Date(2018, 4, 23, 10, 33, 30, 0)
                     };
                     $scope.addEvent(eventData);
                 }
             }
         }          };
+    $scope.addEvent = function (eventData) {
+        console.log(eventData);
+        $scope.calEventsPers.events.push(eventData);
+        console.log($scope.calEventsPers.events);
+    }
 
 });
